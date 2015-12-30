121b3b841c5aea17955fd0fe2a486cbe58e28383
sirinapha88 <sirinapha888@gmail.com>
2015-12-29 16:35:06 -0800

    added css file and changed book router

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/asset/'));
 
 app.use('/authors', authorRouter);
-// app.use('/authors/:author_id/books', bookRouter);
+app.use('/authors/books', bookRouter);
 
 app.listen(3000, function(){
 	console.log("Server started on port 3000");
--- controllers/books.js
+++ controllers/books.js
@@ -3,32 +3,52 @@ var router = express.Router();
 var knex = require('../db/knex');
 var locus = require("locus");
 
-router.get('/',function(req,res){
+router.get('/:author_id',function(req,res){
 	var id = req.params.author_id;
-	knex('books').then(function(books){
-		res.render('books/index',{books:books,id:id});
+	 knex("authors")
+    .innerJoin("books", "books.author_id", "authors.id")
+    .where("author_id", id)
+    .then(function(books) {
+      //  Get the author's name before rendering books page
+      knex("authors").first().where("id", id).then(function(author) {
+        res.render("books/index", {
+          author: author,
+          books : books
+        });
+      })
+    });
+});
+
+router.get('/:author_id/new',function(req,res){
+	var id = req.params.author_id;
+	knex("authors").where({id:id}).first().then(function(author){
+		res.render('books/new', {author:author});
 	});
 });
 
-router.get('/new',function(req,res){
-	var id = req.params.author_id;
-  	res.render('books/new', {id:id});
-});
-
-router.post('/',function(req,res){
+router.post('/:author_id/new',function(req,res){
 	
 	var name = req.body.name;
 	var author_id = req.params.author_id;
 	
-	knex('books').insert({book_name:name, author_id:author_id}).then(function(book){
-		res.redirect('/authors/' + author_id + '/books', {author_id:author_id});
+	knex('books').insert({"book_name":name, "author_id":author_id}).then(function(){
+		res.redirect('/authors');
 	});
 });
 
-router.get('/:id/edit',function(req,res){
+router.get('/:author_id/:id/edit',function(req,res){
 	var id = req.params.id;
+	var author_id = req.params.author_id;
 	knex('books').where({id:id}).first().then(function(book){
-		res.render("books/edit", {book:book});
+		res.render("books/edit", {author_id:author_id,book:book});
+	});
+});
+
+router.put('/:author_id/:id',function(req,res){
+	var id = req.params.id;
+	var author_id = req.params.author_id;
+	knex('books').where({id:id}).first().update(req.body).then(function(){
+		res.redirect('/authors/books/' + author_id);
 	});
 });
 
